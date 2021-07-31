ff94ecceb087a577ee503a9fc7b89004f4541bc3
maen08 <2001stany@gmail.com>
2021-07-31 15:07:06 +0300

    update: ready for publish

--- setup.py
+++ setup.py
@@ -10,51 +10,48 @@ from setuptools import setup
 with open('README.md') as readme:
     long_description = readme.read()
 
-with open('requirements.txt') as requirements_production:
-    install_requires = requirements_production.readlines()
+# with open('requirements.txt') as requirements_production:
+#     install_requires = requirements_production.readlines()
 
 setup(
-    name='',
+    name='tzunipy',
 
-    description='',
+    description='A python package that provides details about Universities of Tanzania',
 
     long_description=long_description,
 
     long_description_content_type='text/markdown',
 
-    version='',    # keep on changing
+    version='0.1',    # keep on changing
 
-    url='https://github.com/maen08/',
+    url='https://github.com/maen08/tzunipy',
 
     download_url='https://github.com/ioi2908/djangoroku/archive/v_01.tar.gz',
 
-    author='',
+    author='Stanley Ruheza',
 
     author_email='2001stany@gmail.com',
 
     license='MIT',
 
-    packages=[''],
+    packages=['tzunipy'],
 
     include_package_data=True,
 
-    install_requires=install_requires,
+    install_requires= [ 'setuptools'],
 
-    keywords=['django', 'heroku', 'deploy django', 'django heroku'],
+    keywords=['tanzania', 'university', 'tanzania-university', 'unipy' , 'tz'],
 
     classifiers=[
        
         'Development Status :: 3 - Alpha',  # keep on increasing
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Framework :: Django',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.x',
     ],
 
     
