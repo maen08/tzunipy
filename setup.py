53a8fe0df74b73752cd70fc512a51b98c10a723a
maen08 <2001stany@gmail.com>
2021-07-31 15:20:37 +0300

    done: first version published

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
 
     url='https://github.com/maen08/tzunipy',
 
-    download_url='https://github.com/ioi2908/djangoroku/archive/v_01.tar.gz',
+    download_url='https://github.com/maen08/tzunipy/archive/refs/tags/v0.1.tar.gz',
 
     author='Stanley Ruheza',
 
